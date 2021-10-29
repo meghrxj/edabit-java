078dd6b4b97c488887602c5c3d247ac358e2087e
meghrxj <hello.meghrajjj@gmail.com>
2021-10-30 00:00:58 +0530

    Update Solution1.java

--- Solution1.java
+++ Solution1.java
@@ -32,7 +32,12 @@ public class Solution1 {
 		return num <= 0;
     }
 
-	//
+	//Maximum Edge of a Triangle
+	//Create a function that finds the maximum range of a triangle's third edge, where the side lengths are all integers.
+	public static int nextEdge(int side1, int side2) {
+			return side1 + side2 - 1;
+	}
+
 
 
 
