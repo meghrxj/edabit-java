ffd6ee1e8a657d81b15fb2cdc94c941990cedb5b
meghrxj <hello.meghrajjj@gmail.com>
2021-10-30 00:11:27 +0530

    Update Solution1

--- Solution1.java
+++ Solution1.java
@@ -45,7 +45,13 @@ public class Solution1 {
 		return ++num;
 
 	}
-
+	//Basic Variable Assignment
+	//A student learning Java was trying to make a function. His code should concatenate a passed string name with string "Edabit" and store it in a variable called result. He needs your help to fix this code.
+	public static String nameString(String name) {
+		return name + "Edabit";
+  }
+  //
+  //
 
 
 }
