958daadb232e95f3e42d94731ac9e5d9824a1320
meghrxj <hello.meghrajjj@gmail.com>
2021-10-29 23:57:57 +0530

    Update Solution1.java

--- Solution1.java
+++ Solution1.java
@@ -24,12 +24,16 @@ public class Solution1 {
 	//Create a method that takes an integer as its only argument and returns true if it's less than or equal to zero, otherwise return false.
 	public static boolean lessThanOrEqualToZero(int num) {
       
-		if(num <= 0){
+		/*if(num <= 0){
 			return true;
 		} else {
 			return false;
-		}
+		} */
+		return num <= 0;
     }
+
+	//
+
 
 
 }
