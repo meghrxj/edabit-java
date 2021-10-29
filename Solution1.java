73412e5e01720209e97c3d9642f75c6bda06f9fc
meghrxj <hello.meghrajjj@gmail.com>
2021-10-29 23:51:19 +0530

    Create Solution1.java

---
+++ Solution1.java
@@ -0,0 +1,19 @@
+public class Solution1 {
+
+    //Upvotes vs Downvotes
+	//Given an object containing counts of both upvotes and downvotes, return what vote count should be displayed. This is calculated by subtracting the number of downvotes from upvotes.
+
+	public static int getVoteCount(int upvotes,int downvotes) {
+		return upvotes - downvotes;
+	}
+	//Find the Perimeter of Rectangle
+	//Create a function that takes length and width and finds the perimeter of a rectangle.
+
+	public static int findPerimeter(int length, int width) {
+		return 2*(width + length);
+
+	}
+
+	//Power Calculator
+
+}
